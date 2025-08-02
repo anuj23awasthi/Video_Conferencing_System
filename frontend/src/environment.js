85facd376a14b615e5811963abcd22e2862e1cf9
Anuj <anujawasthi350@gmail.com>
2025-08-03 01:19:06 +0530

    Ready for Deployment frontend

---
+++ frontend/src/environment.js
@@ -0,0 +1,8 @@
+let IS_PROD = true;
+const servers = IS_PROD ?
+"https://video-conferencing-system-sp4m.onrender.com" :
+
+  "http://localhost:8000"
+   
+
+export default servers;
--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -15,7 +15,7 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import ChatIcon from '@mui/icons-material/Chat';
 
 
-const server_url = "http://localhost:8000";
+const server_url = server.prod;
 
 var connections = {};
 
